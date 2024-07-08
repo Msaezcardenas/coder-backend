6dbd9d23343de892fb451b92dc0191939938357f
molu <msaezcardenas@gmail.com>
2024-07-08 11:27:10 -0400

    add limit to query

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -7,8 +7,14 @@ const productManager = new ProductManager(__dirname + '/data/products.json');
 
 router.get('/', async (req, res) => {
   try {
+    const limit = req.query.limit;
     const productList = await productManager.getProducts();
-    res.status(201).json({ data: productList });
+    if (limit) {
+      const products = productList.slice(0, limit);
+      res.status(201).json({ data: products });
+    } else {
+      res.status(201).json({ data: productList });
+    }
   } catch (error) {
     res.status(400).json({ message: 'Server Error' });
   }
