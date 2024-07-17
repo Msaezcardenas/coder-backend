bb5a0b3d50b977276616e43fe17b7079821c5732
molu <msaezcardenas@gmail.com>
2024-07-16 22:26:28 -0400

    agrego handlebars

--- src/app.js
+++ src/app.js
@@ -1,14 +1,23 @@
 import express from 'express';
 import ProductsRoute from './routes/products.router.js';
 import CartsRoute from './routes/carts.router.js';
+import handlebars from 'express-handlebars';
+import ViewsRouters from './routes/viewsRouters.router.js';
+import { __dirname } from './utils.js';
 
 const app = express();
 const port = 8080;
+
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use('/api/products/', ProductsRoute);
 app.use('/api/carts/', CartsRoute);
+app.use('/', ViewsRouters);
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
